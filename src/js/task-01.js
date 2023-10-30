3c2d78d5cc86201ae36bdb833ac910bd4debe6c3
jarvis1666 <139481899+jarvis1666@users.noreply.github.com>
2023-10-30 23:02:49 +0200

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,13 @@
 `use strict`;
 const elementItem = document.querySelectorAll(".item");
 console.log(`Number of categories: ${elementItem.length}`);
+
+const categoriesArrey = document.querySelectorAll(".item");
+categoriesArrey.forEach((nameCategories) => {
+  const nameCategoriesArrey = nameCategories.querySelectorAll(`h2`);
+  console.log(`Category: ${nameCategoriesArrey[0].textContent}`);
+
+  const arrayCategoriesObject = nameCategories.querySelectorAll(`ul li`);
+  const lengthArrayCategoriesObject = arrayCategoriesObject.length;
+  console.log(`Elements: ${lengthArrayCategoriesObject}`);
+});
